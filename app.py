8756312b3577ae89390fbae5e06aaa7efa3de07c
lartdeschimeres <lartdeschimeres@gmail.com>
2026-01-27 22:57:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -863,16 +863,16 @@ elif st.session_state.page == "army":
                             selected_options[group["group"]].append(o)
                             upgrades_cost += o["cost"]
     
-    # Doublement d'effectif : INTERDIT pour les héros
-    if unit.get("type") != "hero":
-        double_size = st.checkbox(
-            "Doubler les effectifs (+100% coût de base et armes)",
-            value=False
-        )
-    else:
-        double_size = False
+# Doublement d'effectif : INTERDIT pour les héros
+if unit.get("type") != "hero":
+    double_size = st.checkbox(
+        "Doubler les effectifs (+100% coût de base et armes)",
+        value=False
+    )
+else:
+    double_size = False
     
-    multiplier = 2 if double_size else 1
+multiplier = 2 if double_size else 1
                
     # -------------------------------
     # Calcul du coût final
